0b3f759f70b4a76658b53117ef69b434d0dfb9a8
GHTeodor <Teodor15@ukr.net>
2022-03-12 20:37:27 +0200

    TypeORM, Requests

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,51 @@
+import 'reflect-metadata';
 import express, { Request, Response } from 'express';
-
-import { users } from './users';
+import { createConnection, getManager } from 'typeorm';
+import { User } from './entity/user';
 
 const app = express();
-console.log(users);
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.get('/', (req: Request, res: Response) => {
-    res.end();
+app.get('/users', async (req: Request, res: Response) => {
+    const users = await getManager().getRepository(User).find({ relations: ['posts'] });
+    // =================================
+    // const users = await getManager().getRepository(User)
+    //     .createQueryBuilder('user')
+    //     .where('user.firstName = "UA"')
+    //     .getOne();
+    // =================================
+    res.json(users);
 });
 
-const PORT: number = 5200;
-app.listen(PORT, () => console.log(`Server has been started on PORT ${PORT}🚀🚀🚀`));
+app.post('/users', async (req, res) => res.json(await getManager().getRepository(User).save(req.body)));
+
+app.patch('/users/:id', async (req, res) => {
+    const { password, email } = req.body;
+    const createdUser = await getManager()
+        .getRepository(User)
+        .update({ id: +req.params.id }, {
+            password,
+            email,
+        });
+    res.json(createdUser);
+});
+
+app.delete('/users/:id', async (req, res) => {
+    const deletedUser = await getManager()
+        .getRepository(User)
+        // .delete({ id: +req.params.id });
+        .softDelete({ id: +req.params.id });
+    res.json(deletedUser);
+});
+
+const PORT:number = 5200;
+app.listen(PORT, async () => {
+    try {
+        const connection = await createConnection();
+        if (connection) console.log('Database connected');
+    } catch (error) {
+        if (error) console.log(error);
+    }
+    console.log(`Server has been started on port ${PORT} 🚀🚀🚀`);
+});
--- src/users.ts
+++
@@ -1,1 +0,0 @@
-export const users: number[] = [1, 2, 3];

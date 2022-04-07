96c2b51b626e400fc8291e0f3671b1078fc42fa1
GHTeodor <Theodor.Lviv@gmail.com>
2022-04-07 18:16:45 +0300

    Socket

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,56 @@
 import 'reflect-metadata';
 import express from 'express';
 import fileUpload from 'express-fileupload';
+import http from 'http';
 import { createConnection } from 'typeorm';
+import SocketIO from 'socket.io';
 
 import { apiRouter } from './routers/apiRouter';
 import { config } from './configs/config';
 import { cronRun } from './cron';
+import { socketController } from './controllers';
 
 const app = express();
+const server = http.createServer(app);
+// @ts-ignore
+const io = SocketIO(server, { cors: { origin: '*' } });
+
+io.on('connection', (socket: any) => {
+    console.log(socket.handshake.query);
+    console.log(socket.handshake.accessToken);
+
+    socket.on('message:create', (data: any) => socketController.messageCreate(io, socket, data));
+    socket.on('join_room', (data: any) => {
+        socket.join(data.id);
+
+        // ONE TO MANY EXCLUDE SENDER
+        // socket.broadcast.to(data.id).emit('user_join_room', { message: `User ${socket.id} joined room ${data.id}` });
+
+        // EMIT TO ALL USERS IN ROOM (INCLUDE SENDER)
+        io.to(data.id).emit('user_join_room', { message: `User ${socket.id} joined room ${data.id}` });
+    });
+    // --------------------------------------------------------------------------------------------------
+
+    // ONE TO ONE
+    // socket.emit(event, {});
+
+    // SEND TO ALL ONLINE USERS (INCLUDE SENDER)
+    // io.emit(event, {})
+
+    // SEND TO ALL ONLINE USERS (AVOID SENDER)
+    // socket.broadcast.emit(event, {})
+
+    // socket.join(room_id)
+
+    // TO ROOM AVOID SENDER
+    // socket.broadcast.to(room_id).emit(event, {})
+
+    // TO ROOM INCLUDE SENDER
+    // io.to(room_id).emit(event, {})
+
+    // --------------------------------------------------------------------------------------------------
+});
+
 app.use(fileUpload);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,7 +66,7 @@ app.use('*', (err, req, res, next) => {
 });
 
 const { PORT } = config;
-app.listen(PORT, async () => {
+server.listen(PORT, async () => {
     console.log(`Server has been started on port ${PORT} 🚀🚀🚀`);
 
     try {
---
+++ src/controllers/socketController.ts
@@ -0,0 +1,11 @@
+export const socketController = {
+    messageCreate: (io: any, socket: any, data: any) => {
+        // ONE TO ONE
+        // socket.emit('message:get-all', { messages: [{ text: data.message }] });
+
+        // SEND TO ALL ONLINE USERS
+        // io.emit('message:get-all', { messages: [{ text: data.message }] });
+
+        socket.broadcast.emit('message:get-all', 'TEST');
+    },
+};

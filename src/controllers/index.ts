14cb544d93db500eaadd6d138c62a591fc1247c2
GHTeodor <Theodor.Lviv@gmail.com>
2022-05-01 00:32:36 +0300

    Socket

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,6 +2,7 @@ export * from './authController';
 export * from './userController';
 export * from './postController';
 export * from './commentController';
+export * from './socketController';
 
 export * from './studentController'; // MongoDB
 export * from './teacherController'; // MongoDB
